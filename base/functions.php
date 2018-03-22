ee5ee16691419fe1b8116334a58dd59d41679851
Mark Allen Evans <president@worldvaporexpo.com>
2018-03-21 23:58:19 -0400

    Update functions.php

--- base/functions.php
+++ base/functions.php
@@ -29,7 +29,7 @@ function cors() {
 	// Access-Control headers are received during OPTIONS requests
 	if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
 		if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-			header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+			header("Access-Control-Allow-Methods: GET, OPTIONS"); // removed POST / PUT for now
 		if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
 			header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
 		//exit(0);
